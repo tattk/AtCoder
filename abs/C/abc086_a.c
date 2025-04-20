eb647ab8f0d7eec2e52cf8946eab835057887d94
tattk <152263071+tattk@users.noreply.github.com>
2025-04-20 20:59:36 +0900

    Add new AtCoder submissions

--- abs/C/abc086_a.c
+++ abs/C/abc086_a.c
@@ -2,3 +2,14 @@
 // Language: C (gcc 12.2.0)
 // https://atcoder.jp/contests/abs/submissions/57209280
 
+#include<stdio.h>
+int main(){
+    int a,b;
+    
+    scanf("%d %d",&a,&b);
+    
+    if(a*b%2==1)
+        printf("Odd");
+    else
+        printf("Even");
+}
